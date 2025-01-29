2556d4138b1745c36b14262180685527e63913ec
Vinícius Pinto Gonçalves <136902934+vviniciws@users.noreply.github.com>
2025-01-29 14:34:26 -0300

    att

---
+++ IN.sql
@@ -0,0 +1,9 @@
+USE sakila;
+
+SELECT * 
+FROM address
+WHERE district IN ('Alberta', 'Texas', 'California');
+"Filtre os registros da coluna (district) que forem iguais
+a 'Alberta', 'Texas', 'California'"
+
+
