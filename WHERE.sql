0459177c2c6e9a9e41b19196652da7eb9c3aa4bb
Vinícius Pinto Gonçalves <136902934+vviniciws@users.noreply.github.com>
2025-01-29 13:49:08 -0300

    att

---
+++ WHERE.sql
@@ -0,0 +1,13 @@
+USE sakila;
+
+SELECT actor_id, first_name, last_name 
+FROM actor
+"Selecione a coluna (actor_id,first_name, last_name) 
+da tabela (actor)" 
+
+WHERE actor_id <= 10
+"Filtre as colunas (actor_id) que forem menores ou 
+iguais a 10"
+
+
+
