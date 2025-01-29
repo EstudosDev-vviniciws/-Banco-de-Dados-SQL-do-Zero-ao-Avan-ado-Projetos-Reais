1d3375010429c494ed72614ce958cf2c5bb95bf2
Vinícius Pinto Gonçalves <136902934+vviniciws@users.noreply.github.com>
2025-01-29 14:01:15 -0300

    Update Modificando uma Coluna.sql

--- Modificando uma Coluna.sql
+++ Modificando uma Coluna.sql
@@ -5,7 +5,7 @@ SELECT
     amount,
     amount - (amount * 0.10) AS '10 % discount'
     "AS : é usado para adicionar um título a nova coluna"
-    "Essa nova tabela (10 % dicount) é apenas visualizada
+    "Essa nova coluna (10 % dicount) é apenas visualizada
     na consulta e não adicionada/armazenada"
 FROM payment
 WHERE customer_id = 1
