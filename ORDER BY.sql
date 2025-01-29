7dce9bd22455402d2f14c322c23cc3207aaec866
Vinícius Pinto Gonçalves <136902934+vviniciws@users.noreply.github.com>
2025-01-29 13:43:15 -0300

    Update ORDER BY.sql

--- ORDER BY.sql
+++ ORDER BY.sql
@@ -1,4 +1,6 @@
-SELECT actor_id, first_name, last_name from actor
+USE sakila;
+
+SELECT actor_id, first_name, last_name FROM actor
 "Selecione a coluna (actor_id,first_name, last_name) 
 da tabela (actor)" 
 
