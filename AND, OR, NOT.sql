6b26a4c2444c54e5bfe707d16ea1aec4b396253d
Vinícius Pinto Gonçalves <136902934+vviniciws@users.noreply.github.com>
2025-01-29 14:31:43 -0300

    Update AND, OR, NOT.sql

--- AND, OR, NOT.sql
+++ AND, OR, NOT.sql
@@ -2,14 +2,14 @@ USE sakila;
 
 SELECT * FROM payment
 WHERE NOT staff_id = 1 AND amount = 0.99 AND customer_id < 10
-"Filtre os valores da coluna (staff_id) que não forem
-iguais a 1 e os valores da coluna (amount) que forem iguais
-a 0.99 e os valores da coluna costumer_id que forem menores
+"Filtre os registros da coluna (staff_id) que não forem
+iguais a 1 e os registros da coluna (amount) que forem iguais
+a 0.99 e os registros da coluna costumer_id que forem menores
 que 10 "
 
 -- WHERE staff_id = 1 OR amount = 0.99
-"Filtre os valores da colunas (staff_id) que forem
-iguais a 1 ou os valores da coluna (amount) que forem
+"Filtre os registros da colunas (staff_id) que forem
+iguais a 1 ou os registros da coluna (amount) que forem
 iguais a 0.99"
 
 
