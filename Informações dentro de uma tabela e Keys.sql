9ccc323a99a8d6e2a4069312a77700ab422a17ce
Vinícius Pinto Gonçalves <136902934+vviniciws@users.noreply.github.com>
2025-01-29 13:33:38 -0300

    att

---
+++ Informações dentro de uma tabela e Keys.sql
@@ -0,0 +1,13 @@
+TUPLE OR ROW -> "Seriam as Linhas"
+
+COLUMN(ATRIBUTE) -> "Seriam as Colunas, tipo address_id, city_id"
+
+PRIMARY KEY -> "É a Chave Primária (ID) que identifica de
+forma única cada registro dentro da própria tabela."
+
+FOREIGN KEY -> "A Chave Estrangeira é uma PRIMARY KEY de 
+outra tabela que é utilizada para criar um vínculo entre os
+dados, garantindo a integridade referencial entre as tabelas."
+
+
+
