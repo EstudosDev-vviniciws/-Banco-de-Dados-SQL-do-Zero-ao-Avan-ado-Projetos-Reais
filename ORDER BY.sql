75d3b3bd4debc7d371b875c012d35d2ae05781f5
Vinícius Pinto Gonçalves <136902934+vviniciws@users.noreply.github.com>
2025-01-29 13:41:37 -0300

    att

---
+++ ORDER BY.sql
@@ -0,0 +1,8 @@
+SELECT actor_id, first_name, last_name from actor
+"Selecione a coluna (actor_id,first_name, last_name) 
+da tabela (actor)" 
+
+ORDER BY last_name
+"Ordene a coluna (last_name)"
+
+
