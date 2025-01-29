9ffe54f7e458729da7b2e9de13e6e94745a7f3a7
Vinícius Pinto Gonçalves <136902934+vviniciws@users.noreply.github.com>
2025-01-29 16:27:32 -0300

    Delete LIKE.sql

--- LIKE.sql
+++
@@ -1,9 +0,0 @@
-USE sakila;
-
-SELECT *
-FROM actor
-WHERE first_name LIKE 'A%';
-"Filtre os registros da coluna (first_name)
-que comecem com a letra(a)"
-"OBS: É usado % para indicar que começa com a 
-letra (a) e não que apenas tenha letra (a) no nome"
