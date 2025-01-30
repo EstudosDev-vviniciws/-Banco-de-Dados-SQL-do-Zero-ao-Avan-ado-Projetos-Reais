786f2251e977c7c50f70a8a0932722119193b6a8
Vinícius Pinto Gonçalves <136902934+vviniciws@users.noreply.github.com>
2025-01-30 11:02:59 -0300

    Delete SQL/Utilizando Inner JOIN.sql

--- SQL/Utilizando Inner JOIN.sql
+++
@@ -1,9 +0,0 @@
-USE sakila;
-
-SELECT * 
-FROM customer
-JOIN payment ON customer.customer_id = payment.payment_id
-"Junte as tabelas (customer e payment), onde a tabela
-costumer, pega os registros da coluna costumer_id e eles 
-tem que serem iguais aos registros da coluna payment_id
-na tabela payment"
