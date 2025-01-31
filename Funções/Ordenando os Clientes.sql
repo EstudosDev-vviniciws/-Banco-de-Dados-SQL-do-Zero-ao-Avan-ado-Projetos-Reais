7ec7f8ab8182e55b2a8275138822dbd46712673e
Vinícius Pinto Gonçalves <136902934+vviniciws@users.noreply.github.com>
2025-01-31 10:48:04 -0300

    Update Ordenando os Clientes.sql

--- Funções/Ordenando os Clientes.sql
+++ Funções/Ordenando os Clientes.sql
@@ -2,9 +2,9 @@ USE sakila;
 
 SELECT 
 	cus.customer_id AS ID,
-    cus.first_name AS Nome,
-    cus.last_name AS Sobrenome,
-    SUM(amount) AS Total
+        cus.first_name AS Nome,
+        cus.last_name AS Sobrenome,
+        SUM(amount) AS Total
     
 FROM payment pay
 JOIN customer cus USING(customer_id)
